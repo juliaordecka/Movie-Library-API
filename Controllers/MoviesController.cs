554d0bfaef31684779992b5ef18304e45e29b855
juliaordecka <juliaordecka@gmail.com>
2025-06-02 05:15:14 +0200

    Dodaj pliki projektów.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieLibrary.Application.Repositories;
+
+namespace Movie_library.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+
+        }
+    }
+}
